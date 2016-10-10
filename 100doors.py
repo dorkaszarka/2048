29891596a8ab25753351ae18fac9ea1c3fedb1a0
dorkaszarka <dorka.szarka@gmail>
2016-10-10 14:15:44 +0200

    Ready

---
+++ 100doors.py
@@ -0,0 +1,17 @@
+hallway = [0] * 100
+step = 1
+
+while step <= len(hallway):
+    door = step - 1
+    while door < len(hallway):
+        if hallway[door] == 0:
+            hallway[door]= 1
+        else:
+            hallway[door] = 0
+        door = door + step
+    step = step + 1
+
+for door in range (0, len(hallway)):
+    if hallway[door] == 1:
+        print("Open", door+1)
+
